bc3b089849a91d2f24f5dc8ac5ae00cc5763d7f6
stylelist <wqhjwhjlweasdrdgsdgffg@gmail.com>
2024-04-11 13:15:25 +0300

    Начинает проект

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import { Banner } from "./components/Banner/Banner";
+import { PopularCardsList } from "./components/CardsList/PopularCardsList";
+import { NewCardsList } from "./components/CardsList/NewCardsList";
+import { Promo } from "./components/Promo/Promo";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <Banner />
+      <PopularCardsList />
+      <NewCardsList />
+      <Promo />
+    </main>
+  );
+}
