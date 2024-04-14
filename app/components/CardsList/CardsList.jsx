d65d0a437c902e261d11c0eda4e4a93554f2567a
stylelist <wqhjwhjlweasdrdgsdgffg@gmail.com>
2024-04-14 17:25:30 +0300

    Создаёт компонент списка и выделяет компонент карточки

---
+++ app/components/CardsList/CardsList.jsx
@@ -0,0 +1,27 @@
+import { Card } from "../Card/Card";
+import Styles from "./CardsList.module.css";
+
+export const CardsList = (props) => {
+  return (
+    <section className={Styles["list-section"]}>
+      <h2 className={Styles["list-section__title"]} id={props.id}>
+        {props.title}
+      </h2>
+      <ul className={Styles["cards-list"]}>
+        {props.data.map((item) => {
+          return (
+            <li className={Styles["cards-list__item"]} key={item.id}>
+              <a
+                href={item.link}
+                target="_blank"
+                className={Styles["card-list__link"]}
+              >
+                <Card {...item} />
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
+import { getGamesByCategory } from "./data/data-utils";
+
 import { Banner } from "./components/Banner/Banner";
-import { PopularCardsList } from "./components/CardsList/PopularCardsList";
-import { NewCardsList } from "./components/CardsList/NewCardsList";
+import { CardsList } from "./components/CardsList/CardsList";
 import { Promo } from "./components/Promo/Promo";
 
 export default function Home() {
+  const popularGames = getGamesByCategory("popular");
+  const newGames = getGamesByCategory("new");
   return (
     <main className="main">
       <Banner />
-      <PopularCardsList />
-      <NewCardsList />
+      <CardsList id="popular" title="Популярные" data={popularGames} />
+      <CardsList id="new" title="Новинки" data={newGames} />
       <Promo />
     </main>
   );
