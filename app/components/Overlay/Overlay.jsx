97b436668b7c68bfaf6170b3015febab4366463b
stylelist <wqhjwhjlweasdrdgsdgffg@gmail.com>
2024-04-17 21:03:29 +0300

    Добавляет возможность закрывать окно авторизации

--- app/components/Overlay/Overlay.jsx
+++ app/components/Overlay/Overlay.jsx
@@ -3,7 +3,10 @@ import Styles from "./Overlay.module.css";
 export const Overlay = (props) => {
   return (
     <div
-      className={`${Styles["overlay"]} ${Styles["overlay_is-opened"]}`}
+      className={`${Styles["overlay"]} ${
+        props.isOpened && Styles["overlay_is-opened"]
+      }`}
+      onClick={() => props.close()}
     ></div>
   );
 };
